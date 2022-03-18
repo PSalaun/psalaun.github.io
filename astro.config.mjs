16d8e61550a1ddde3c9e3780a53e67007fc53f08
PSalaun <pierresalaun10@hotmail.com>
2022-03-18 10:23:07 +0100

    improve import alias

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,24 +1,7 @@
 import {defineConfig} from 'astro/config';
-import path from 'path';
-import { fileURLToPath } from 'url';
-
-/*
-	define filename and dirname in ESM scope
-	https://bobbyhadz.com/blog/javascript-dirname-is-not-defined-in-es-module-scope
- */
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 // https://astro.build/config
 export default defineConfig({
 	// Enable the Preact renderer to support Preact JSX components.
 	renderers: ['@astrojs/renderer-preact'],
-	vite: {
-		// Enable absolute imports in Vite
-		resolve: {
-			alias: [
-				{ find: 'src', replacement: path.resolve(__dirname, 'src') },
-			],
-		},
-	}
 });
