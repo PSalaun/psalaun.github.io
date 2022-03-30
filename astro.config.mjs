ac608aa47e704a99649ec5524f59becd0d88bcd1
PSalaun <pierresalaun10@hotmail.com>
2022-03-30 19:05:08 +0200

    add astro image plugn

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,11 @@
 import {defineConfig} from 'astro/config';
+import astroImagePlugin from 'astro-imagetools/plugin';
 
 // https://astro.build/config
 export default defineConfig({
 	// Enable the Preact renderer to support Preact JSX components.
 	renderers: ['@astrojs/renderer-preact'],
+	vite: {
+		plugins:[astroImagePlugin],
+	}
 });
