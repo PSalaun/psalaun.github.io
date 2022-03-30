c0db6abadf204431be4d0448aad7ad41330fe5bf
PSalaun <pierresalaun10@hotmail.com>
2022-03-30 19:43:25 +0200

    add global font and color

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,5 +3,15 @@ module.exports = {
     './public/**/*.html',
     './src/**/*.{astro,js,jsx,svelte,ts,tsx,vue}',
   ],
+  theme: {
+    fontFamily: {
+      ui: ['Arial', 'sans-serif'],
+      sans: ['Trebuchet MS', 'sans-serif'],
+    },
+    colors: {
+      'dark': '#44403C',
+      'light': '#FAFAF9',
+    }
+  }
   // more options here
 };
