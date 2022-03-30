f1cd0b69bd4ae81300bdc27bd7f12653fdadb1a1
PSalaun <pierresalaun10@hotmail.com>
2022-03-30 21:52:52 +0200

    improve homepage responsiveness

--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -2,8 +2,8 @@ import { h } from 'preact';
 
 const Nav = () => {
 	return (
-		<nav class="flex justify-end py-3 px-16">
-			<div class="text-xl flex gap-4">
+		<nav class="flex justify-end py-3 px-12 lg:px-16">
+			<div class="text-lg lg:text-xl flex gap-4">
 				<a href="/">accueil</a>
 				<a href="/portfolio">portfolio</a>
 				<a href="/philosophie">ma philosophie</a>
