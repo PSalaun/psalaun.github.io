aa6ee157051b368d788566363c67ce7c0bb12528
PSalaun <pierresalaun10@hotmail.com>
2022-03-31 11:31:28 +0200

    fix layout shift on navbar

--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -1,20 +1,20 @@
 import { h } from 'preact';
+import Styles from './styles.module.scss';
 
 const Nav = ({currentPath}) => {
-	const isActive = (path) => {
-		return currentPath === path;
+	const isActive = (path, current) => {
+		return current === path;
 	}
 	const links = [
 		{ label: 'accueil', path: '/'},
 		{ label: 'portfolio', path: '/portfolio'},
 		{ label: 'ma philosophie', path: '/philosophie'},
 	];
-	console.log(currentPath)
 	return (
 		<nav class="flex justify-center lg:justify-end py-3 px-8 lg:px-16">
 			<div class="text-lg lg:text-xl flex gap-4">
 				{links.map(link => (
-					<a href={link.path} class={isActive(link.path) ? 'font-bold' : ''}>{link.label}</a>
+					<a data-text={link.label} href={link.path} class={isActive(link.path, currentPath) ? Styles.menuitemActive : Styles.menuitem}>{link.label}</a>
 				))}
 			</div>
 		</nav>
