33513945c4c27d1f8e9c45d24545f6e4f9535a6c
PSalaun <pierresalaun10@hotmail.com>
2022-03-31 11:31:56 +0200

    add more details on footer, and add github link

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -4,9 +4,9 @@ import Button from '@components/Button/index.jsx';
 function Footer() {
 	return (
 		<footer class="flex flex-wrap lg:justify-end py-6 px-12 lg:px-16 text-xl mt-24 gap-4">
-			<span class="flex">Créé en 2022 avec <a class="ml-2" href="https://astro.build" target="_blank" rel="noopener noreferrer"><Button type="secondary" icon="external">Astro</Button></a></span>
+			<span class="flex">Créé en 2022 avec Astro, Preact et TailwindCSS.</span>
 			<span>Hébergé par Github.</span>
-			<a href=""><Button type="secondary" icon="external">Voir le code de ce site</Button></a>
+			<a href="https://github.com/PSalaun/portfolio"><Button type="secondary" icon="external">Voir le code de ce site</Button></a>
 		</footer>
 	);
 }
